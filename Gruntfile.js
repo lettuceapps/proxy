45ec4cc66379a1d9cdb5ac05b7afe86f7ba06ac3
Viphak Lay <viphak@gmail.com>
2013-05-06 18:19:52 -0700

    clean up

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,45 +7,28 @@ var path    = require('path'),
 
 module.exports = function(grunt) {
 
-// var server = require('./app/server.js');
-// var serverPort = server.app.get('port');
-var serverPort = 443;
+  //NODE_ENV=production
+  
+  var serverPort = 443;
 
   grunt.initConfig({
     express: {
-    custom: {
-        options: {
-          watchChanges: true,
-          keepaline: true,
-          port: serverPort,
-          bases: 'www-root',
-          server: path.resolve('./app/server')
+      custom: {
+          options: {
+            watchChanges: true,
+            keepaline: true,
+            port: serverPort,
+            bases: 'www-root',
+            server: path.resolve('./app/server')
+        }
       }
     }
-    }
   });
-
-
 
   grunt.loadNpmTasks('grunt-express');
 
   grunt.registerTask('server', ['express', 'express-keepalive']);
 
-//NODE_ENV=production
-
-  // grunt.registerTask('server', 'Start the appllication web server.', function() {
-
-  //   var options = {
-  //       key: fs.readFileSync(path.resolve('./ssl_key/lettuceapps.com.key')),
-  //       cert: fs.readFileSync(path.resolve('./ssl_key/lettuceapps.com.crt'))
-  //   };
-
-  //   https.createServer(options, server).listen(port, function(){
-  //     grunt.log.writeln('Starting web server on port ' + port + '.');
-  //   });
-
-  // });
-
-
   grunt.registerTask('default', ['server']);
+  
 }
